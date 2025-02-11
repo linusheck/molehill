19b587067da6455c39d638593ffdb50c8b5dda00
Linus Heck <linus.heck@ru.nl>
2025-02-11 11:15:04 +0100

    add benchmark script

--- molehill/counterexamples.py
+++ molehill/counterexamples.py
@@ -39,38 +39,46 @@ def check(matrix_generator, choice_to_assignment, family, prop, global_hint=None
         # we abstract in the order of which holes we saw first, which holes we saw second, etc
         abstracted_holes = hole_order(bfs_order, choice_to_assignment, fixed_holes)
 
-        all_schedulers_violate = False
-        hint = None
-        hint_obj = ExplicitModelCheckerHintDouble()
+        def check_submodel(ith_hole, abstracted_holes):
+            abstracted_holes_here = abstracted_holes[ith_hole:]
 
-        # TODO do binary search for the right number of abstracted holes
-        while not all_schedulers_violate:
             # try to get an unsat core!!
             # let's start with abstracting all of the nondeterminism into holes
             matrix_generator.build_submodel(
-                BitVector(family.num_holes, abstracted_holes), hole_options
+                BitVector(family.num_holes, abstracted_holes_here), hole_options
             )
             mdp_holes = matrix_generator.get_current_mdp()
 
-            hint_obj = None
-            if hint is not None:
-                reachable_states = matrix_generator.get_current_reachable_states()
-                hint_obj = hint_convert(hint, old_reachable_states, reachable_states)
+            # TODO re-implement hint
+            # hint_obj = None
+            # if hint is not None:
+            #     reachable_states = matrix_generator.get_current_reachable_states()
+            #     hint_obj = hint_convert(hint, old_reachable_states, reachable_states)
 
-            all_schedulers_violate, result = check_model(mdp_holes, prop, hint_obj)
-            print(result.get_values()[mdp_holes.initial_states[0]])
-            hint = result.get_values()
+            all_schedulers_violate, result = check_model(mdp_holes, prop, None)
+            # print(result.get_values()[mdp_holes.initial_states[0]])
+            # hint = result.get_values()
 
             if all_schedulers_violate:
                 # yaay we have a counterexample!!
                 counterexample_holes = [
-                    hole for hole in fixed_holes if hole not in abstracted_holes
+                    hole for hole in fixed_holes if hole not in abstracted_holes_here
                 ]
                 return all_schedulers_violate, counterexample_holes, result
             # abstract fewer holes
-            # TODO check if the abstracted hole actually removes nondeterminism
-            abstracted_holes = abstracted_holes[1:]
-            if len(abstracted_holes) == 0:
-                break
-            old_reachable_states = matrix_generator.get_current_reachable_states()
+            # old_reachable_states = matrix_generator.get_current_reachable_states()
+            return None
+
+        # do a binary search to find the smallest counterexample
+        # TODO we don't need to search on the holes that we know are not reachable
+        left = 0
+        right = len(abstracted_holes)
+        while left < right:
+            mid = (left + right) // 2
+            result = check_submodel(mid, abstracted_holes)
+            if result is None:
+                left = mid + 1
+            else:
+                _all_schedulers_violate, fixed_holes, result = result
+                right = mid
     return all_schedulers_violate_full, fixed_holes, result
--- molehill/modelchecker.py
+++ molehill/modelchecker.py
@@ -4,12 +4,12 @@ from stormpy.core import parse_properties_without_context
 from stormpy import check_model_sparse
 from pycarl.gmp import Rational
 
-def check_model(mdp, prop, hint, precision=1e-6):
+def check_model(mdp, prop, hint, precision=1e-4):
     exact_environment = stormpy.core.Environment()
     exact_environment.solver_environment.minmax_solver_environment.precision = Rational(
         precision
     )
-    # exact_environment.solver_environment.minmax_solver_environment.method = stormpy.MinMaxMethod.optimistic_value_iteration
+    exact_environment.solver_environment.minmax_solver_environment.method = stormpy.MinMaxMethod.optimistic_value_iteration
     # exact_environment.solver_environment.minmax_solver_environment.method = stormpy.MinMaxMethod.sound_value_iteration
 
     # TODO hack (i hate properties)
