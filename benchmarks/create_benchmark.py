ffd097841bef2f31d975760862a2a2ca4f680083
Linus Heck <linus.heck@ru.nl>
2025-02-18 15:52:41 +0100

    Finally setup benchexec correctly

---
+++ benchmarks/create_benchmark.py
@@ -0,0 +1,26 @@
+import argparse
+
+argument_parser = argparse.ArgumentParser(description="Create a new benchmark")
+argument_parser.add_argument("input_file", type=str, help="Path to the input file")
+argument_parser.add_argument("property_file", type=str, help="Path to the property file")
+argument_parser.add_argument("expected_verdict", type=str, help="Expected verdict of the property")
+
+args = argument_parser.parse_args()
+input_file = args.input_file
+property_file = args.property_file
+expected_verdict = args.expected_verdict
+
+template = f"""
+format_version: "2.0"
+
+input_files:
+- {{input_file}}
+
+properties:
+  - property_file: {{property_file}}
+    expected_verdict: {{expected_verdict}}
+"""
+
+name = input_file.split("/")[-1].split(".")[0]
+with open(f"files/{name}.yml", "w") as f:
+    f.write(template.format(input_file=input_file, property_file=property_file, expected_verdict=expected_verdict))
---
+++ benchmarks/create_benchmarks.sh
@@ -0,0 +1,17 @@
+python create_benchmark.py ../resources/benchmark/jair/sat_dpm sat.txt true
+
+for i in sat_dpm sat_grid sat_grid-10-sl-4fsc sat_grid-meet-sl-2fsc sat_herman sat_maze sat_maze-sl-5 sat_pole sat_pole-res sat_refuel-06-res ; do
+    python create_benchmark.py ../../resources/benchmark/jair/$i ../sat.txt true
+done
+
+for i in sat_network sat_network-priorities sat_samplerocks dontknow_hallway dontknow_crypt dontknow_drone dontknow_network-priorities ; do
+    python create_benchmark.py ../../resources/benchmark/pomdps/$i ../sat.txt true
+done
+
+for i in unsat_dpm unsat_grid unsat_grid-10-sl-4fsc unsat_grid-meet-sl-2fsc unsat_herman unsat_maze unsat_maze-sl-5 unsat_pole unsat_pole-res unsat_refuel-06-res ; do
+    python create_benchmark.py ../../resources/benchmark/jair/$i ../unsat.txt false
+done
+
+for i in unsat_network unsat_samplerocks ; do
+    python create_benchmark.py ../../resources/benchmark/pomdps/$i ../unsat.txt false
+done
