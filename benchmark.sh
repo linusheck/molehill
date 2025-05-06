4b4a3729fdf90acb1866d90774014237435342ee
Linus Heck <linus.heck@ru.nl>
2025-05-06 13:30:48 +0200

    Dated results folders

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # Call this script with argument "simple" or "robust" 
-python3 -m benchexec.benchexec "comparison_$1.xml" --numOfThreads 32
-PYTHONPATH=$(pwd) python3 $(which table-generator) $(ls results/* | grep ".*results.[^.]*\.xml.bz2")  --all-columns -x benchmarks/tablegenerator.xml -o results/
+
+# Get current datetime in format YYYY-MM-DD_HH-MM-SS
+RESULT_DIR="results/$1-$(date +"%Y-%m-%d_%H-%M-%S")"
+mkdir -p "$RESULT_DIR"
+
+python3 -m benchexec.benchexec "comparison_$1.xml" --numOfThreads $2 --outputpath "$RESULT_DIR"
+PYTHONPATH=$(pwd) python3 $(which table-generator) $(ls $RESULT_DIR/* | grep ".*results.[^.]*\.xml.bz2")  --all-columns -x benchmarks/tablegenerator.xml -o $RESULT_DIR
