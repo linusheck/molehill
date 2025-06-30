783df8ab7bf6fa30d875022b9f4f4aa26c057336
Linus Heck <linus.heck@ru.nl>
2025-06-30 16:37:41 +0200

    Update benchmark XML files

--- benchmark.sh
+++ benchmark.sh
@@ -6,6 +6,4 @@ mkdir -p "$RESULT_DIR"
 
 python3 -m benchexec.benchexec "comparison_$1.xml" --numOfThreads $2 --outputpath "$RESULT_DIR"
 
-# RESULT_DIR="stored_results/simple-2025-05-06_14-15-28/"
-
 ./plot.sh $RESULT_DIR
