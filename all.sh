640a18525f466b4160d717804b54a9d15204f76a
Linus Heck <linus.heck@ru.nl>
2025-07-24 14:59:57 +0000

    all.sh

--- all.sh
+++ all.sh
@@ -3,3 +3,13 @@
 ./benchmark.sh robust_trees 26
 ./benchmark.sh cost 26
 ./benchmark.sh trees 26
+./benchmark.sh simple 26
+./benchmark.sh robust 26
+./benchmark.sh robust_trees 26
+./benchmark.sh cost 26
+./benchmark.sh trees 26
+./benchmark.sh simple 26
+./benchmark.sh robust 26
+./benchmark.sh robust_trees 26
+./benchmark.sh cost 26
+./benchmark.sh trees 26
