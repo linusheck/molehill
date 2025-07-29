4f6bc65fb6f04dbc9eb0a257815b47b981211eaf
Linus Heck <linus.heck@ru.nl>
2025-07-29 15:11:18 +0000

    scale up dpm

--- all.sh
+++ all.sh
@@ -1,15 +1,21 @@
 ./benchmark.sh simple 26
-./benchmark.sh robust 26
-./benchmark.sh robust_trees 26
-./benchmark.sh cost 26
-./benchmark.sh trees 26
 ./benchmark.sh simple 26
-./benchmark.sh robust 26
-./benchmark.sh robust_trees 26
-./benchmark.sh cost 26
-./benchmark.sh trees 26
 ./benchmark.sh simple 26
-./benchmark.sh robust 26
+./benchmark.sh simple 26
+# ./benchmark.sh robust 26
 ./benchmark.sh robust_trees 26
-./benchmark.sh cost 26
+./benchmark.sh robust_trees 26
 ./benchmark.sh trees 26
+./benchmark.sh trees 26
+# # ./benchmark.sh cost 26
+# ./benchmark.sh trees 26
+# ./benchmark.sh simple 26
+# ./benchmark.sh robust 26
+# ./benchmark.sh robust_trees 26
+# ./benchmark.sh cost 26
+# ./benchmark.sh trees 26
+# ./benchmark.sh simple 26
+# ./benchmark.sh robust 26
+# ./benchmark.sh robust_trees 26
+# ./benchmark.sh cost 26
+# ./benchmark.sh trees 26
