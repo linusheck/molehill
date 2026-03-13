43e4308a5026d7f9830066488f913ab0ac51cf12
Linus Heck <linus.heck@ru.nl>
2026-03-13 13:55:39 +0100

    fix tablegenerator

--- scripts/causality.sh
+++ scripts/causality.sh
@@ -2,4 +2,4 @@
 
 RESULT_DIR=$1
 
-PYTHONPATH=$(pwd) python3 $(which table-generator) $(find $RESULT_DIR -type f | grep ".*results\..*\.xml\.bz2$") --all-columns -x benchmarks/tablegenerator.xml -o $RESULT_DIR
+PYTHONPATH=$(pwd) python3 $(which table-generator) $(find $RESULT_DIR -type f | grep ".*results\..*\.xml\.bz2$") --all-columns -x benchmarks/tablegenerator_cause.xml -o $RESULT_DIR
